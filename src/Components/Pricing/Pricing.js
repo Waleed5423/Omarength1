282885b8aa3bf853c9e30d48c381d03bfd7de66a
Waleed5423 <waleed54234@gmail.com>
2025-06-29 20:51:03 +0500

    done2

--- src/Components/Pricing/Pricing.js
+++ src/Components/Pricing/Pricing.js
@@ -1,4 +1,4 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import "./Pricing.css";
@@ -32,6 +32,16 @@ const pricingPlans = [
 ];
 
 const Pricing = forwardRef(({ pricingRef, pricing2Ref }, ref) => {
+  const [showAnimation, setShowAnimation] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowAnimation(true);
+    }, 4500); // Delay for 5 seconds
+
+    return () => clearTimeout(timer); // Cleanup
+  }, []);
+
   const containerVariants = {
     hidden: { opacity: 0 },
     visible: {
@@ -46,63 +56,59 @@ const Pricing = forwardRef(({ pricingRef, pricing2Ref }, ref) => {
   };
 
   return (
-    <>
-      <section className="bg-black" id="pricing" ref={pricingRef}>
-        <motion.div
-          ref={pricing2Ref}
-          className="service-heading text-center text-uppercase"
-          style={{ paddingTop: "3rem" }}
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.2 }}
-          variants={containerVariants}
-        >
-          <motion.h6 variants={cardVariants}>Pricing</motion.h6>
-          <motion.h3 className="text-light" variants={cardVariants}>
-            What We Can Offer
-          </motion.h3>
-        </motion.div>
-        <motion.div
-          className="pricing-plans d-md-flex d-block"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.2 }}
-          variants={containerVariants}
-        >
-          {pricingPlans.map((plan, index) => (
-            <motion.div
-              key={index}
-              className={`pricing-card mx-md-3 my-md-2  mt-4 ${plan.type.toLowerCase()}`}
-              variants={cardVariants}
+    <section className="bg-black" id="pricing" ref={pricingRef}>
+      <motion.div
+        ref={pricing2Ref}
+        className="service-heading text-center text-uppercase"
+        style={{ paddingTop: "1.5rem" }}
+        initial="hidden"
+        animate={showAnimation ? "visible" : "hidden"}
+        variants={containerVariants}
+      >
+        <motion.h6 variants={cardVariants}>Pricing</motion.h6>
+        <motion.h3 className="text-light" variants={cardVariants}>
+          What We Can Offer
+        </motion.h3>
+      </motion.div>
+      <motion.div
+        className="pricing-plans d-md-flex d-block"
+        initial="hidden"
+        animate={showAnimation ? "visible" : "hidden"}
+        variants={containerVariants}
+      >
+        {pricingPlans.map((plan, index) => (
+          <motion.div
+            key={index}
+            className={`pricing-card mx-md-3 my-md-2 mt-4 ${plan.type.toLowerCase()}`}
+            variants={cardVariants}
+          >
+            <div className="heading pt-2">
+              <h4>{plan.type}</h4>
+              <p>{plan.description}</p>
+            </div>
+            <p className="price">
+              {plan.price}
+              <sub>{plan.period}</sub>
+            </p>
+            <ul className="features mt-2">
+              {plan.features.map((feature, i) => (
+                <li key={i} className="fs-6">
+                  <i className="fa-solid fa-check"></i>
+                  <strong>{feature.split(" ")[0]}</strong>{" "}
+                  {feature.split(" ").slice(1).join(" ")}
+                </li>
+              ))}
+            </ul>
+            <Link
+              to={`/package-details/${plan.type.replace(" Package", "")}`}
+              className="cta-btn text-decoration-none"
             >
-              <div className="heading pt-2">
-                <h4>{plan.type}</h4>
-                <p>{plan.description}</p>
-              </div>
-              <p className="price">
-                {plan.price}
-                <sub>{plan.period}</sub>
-              </p>
-              <ul className="features mt-2">
-                {plan.features.map((feature, i) => (
-                  <li key={i} className="fs-6">
-                    <i className="fa-solid fa-check"></i>
-                    <strong>{feature.split(" ")[0]}</strong>{" "}
-                    {feature.split(" ").slice(1).join(" ")}
-                  </li>
-                ))}
-              </ul>
-              <Link
-                to={`/package-details/${plan.type.replace(" Package", "")}`}
-                className="cta-btn text-decoration-none"
-              >
-                SELECT
-              </Link>
-            </motion.div>
-          ))}
-        </motion.div>
-      </section>
-    </>
+              SELECT
+            </Link>
+          </motion.div>
+        ))}
+      </motion.div>
+    </section>
   );
 });
 
